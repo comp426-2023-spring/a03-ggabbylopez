a4f259238c079418e0fec62a173b942f27c86fd1
ggabbylopez <gml0426@email.unc.edu>
2023-04-29 21:43:33 -0400

    Finish first draft of rps-cli

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,3 +1,36 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
+import { playRPS,helpRPS,rulesRPS } from "../lib/rpsls";
+
+const args = minimist(process.argv.slice(2), {
+    alias: {
+        r: "rules"
+    }
+});
+
+if (args.h || args.help){
+    helpRPS();
+    process.exit()
+} else if (args.r || args.rules){
+    rulesRPS();
+    process.exit();
+} else {
+    var playerMove = args._[0];
+
+    if(!playerMove){
+        var result = {"player":"rock"};
+        console.log(JSON.stringify(result));
+        process.exit();
+    }
+
+    playerMove = playerMove.toLowerCase();
+
+    var result = playRPS(playerMove);
+
+    if(!(typeof result == "undefined")){
+        console.log(JSON.stringify(result));
+        process.exit();
+    }
+    process.exit();
+}
